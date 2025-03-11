e125aa2b7da4e3d7d416bdbcae501323f541722c
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2025-03-11 23:41:35 +0600

    ui(hero): hero section is now responsive

--- app/page.js
+++ app/page.js
@@ -1,70 +1,10 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Hero />
-      <div className="min-h-screen h-fit w-full bg-[#010C0B] flex justify-center items-center">
-        <div className="w-full h-fit flex-grow">
-          <div className="min-h-36 h-fit text-center text-5xl font-semibold text-white">
-            Healthcare shouldn’t be complicated. Pulse is a powerful, easy-
-            <br />
-            to-use platform designed to make medical appointments <br />
-            seamless for everyone.
-          </div>
-          <div className="flex  h-80 justify-center items-center my-4 gap-x-2 gap-y-2 w-full flex-wrap">
-            <div className="w-[600px] flex min-w-72 rounded-3xl h-72 bg-[#EBFBCF]">
-              <div className="w-1/2 flex-1 h-full">
-                <div className="flex  w-full h-full flex-col justify-center items-start ml-4 gap-y-5">
-                  <div className="h-14 text-5xl font-semibold text-black text-start">
-                    For Patients
-                  </div>
-                  <div className="h-fit text-[18px] font-normal text-black text-start">
-                    book instantly, and manage your
-                    <br /> appointments—all in one place.
-                  </div>
-                  <Button className="rounded-3xl h-10 w-32 text-sm font-semibold bg-white text-black">
-                    Read More
-                  </Button>
-                </div>
-              </div>
-              <div className="w-full flex-1 h-full relative">
-                <Image
-                  src="/images/about-patient.png"
-                  alt="Patient"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-            <div className="w-[600px] flex min-w-72 rounded-3xl h-72 bg-[#EBFBCF]">
-              <div className="w-1/2 flex-1 h-full">
-                <div className="flex  w-full h-full flex-col justify-center items-start ml-4 gap-y-5">
-                  <div className="h-14 text-5xl font-semibold text-black text-start">
-                    For Doctors
-                  </div>
-                  <div className="h-fit text-[18px] font-normal text-black text-start">
-                    Get a personalized booking page, streamline your schedule,
-                    and focus on what you do best: caring for patients.
-                  </div>
-                  <Button className="rounded-3xl h-10 w-32 mt-5 text-sm font-semibold bg-white text-black">
-                    Read More
-                  </Button>
-                </div>
-              </div>
-              <div className="w-full flex-1 h-full relative">
-                <Image
-                  src="/images/about-doctor.png"
-                  alt="Patient"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <About />
     </>
   );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-[#EBFBCF] flex flex-col relative">
+    <div className="w-full h-fit min-h-screen bg-[#EBFBCF] flex flex-col relative">
       <div className="absolute inset-0">
         <Image
           src="/icons/eclipse.svg"
@@ -13,21 +14,43 @@ const Hero = () => {
         />
       </div>
 
-      <div className="h-24 flex justify-between items-center px-20 relative z-10">
-        <Image src="/icons/logo.svg" alt="Pulse" width={200} height={100} />
-        <Button className="bg-black text-white text-xl font-semibold w-40 h-14">
+      <div className="h-24 flex justify-between items-center px-5 sm:px-20 relative z-10">
+        {/* Responsive Logo */}
+        <Image
+          src="/icons/logo.svg"
+          alt="Pulse"
+          width={200}
+          height={100}
+          className="hidden md:block"
+        />
+        <Image
+          src="/icons/logo.svg"
+          alt="Pulse"
+          width={100}
+          height={50}
+          className="hidden sm:block md:hidden"
+        />
+        <Image
+          src="/icons/logo.svg"
+          alt="Pulse"
+          width={80}
+          height={40}
+          className="block sm:hidden"
+        />
+        <Button className="bg-black text-white text-xs sm:text-base md:text-xl font-semibold w-16 sm:w-24 md:w-40 h-9 sm:h-10 md:h-14">
           Use Pulse
         </Button>
       </div>
 
-      <div className="flex-grow flex justify-between items-center px-20 relative z-10">
-        <div className="w-1/2 space-y-6">
-          <div className="text-5xl flex flex-col  gap-y-3 font-bold text-gray-900">
-            <div>Your Time Matters. </div>
-            <div> Healthcare Should</div>
+      <div className="flex-grow flex flex-col md:flex-row justify-between items-center px-5 my-5 sm:px-20 relative z-10">
+        {/* Left Content */}
+        <div className="w-full md:w-1/2 space-y-6">
+          <div className="text-3xl md:text-5xl flex justify-center md:justify-start items-center md:items-start flex-col gap-y-3 font-bold text-gray-900">
+            <div>Your Time Matters.</div>
+            <div>Healthcare Should</div>
             <div>Be Simple.</div>
           </div>
-          <p className="text-lg  text-[#010C0B]">
+          <p className="text-base md:text-lg text-center md:text-start text-[#010C0B]">
             <div>
               Tired of endless phone calls and clunky scheduling? Pulse bridges
             </div>
@@ -36,36 +59,70 @@ const Hero = () => {
             </div>
             <div>intuitive platform.</div>
           </p>
-          <Button className="bg-[#BEF264] text-black w-56 h-16 text-lg font-bold">
-            <div>Use Pulse</div>
-            <Image
-              src="/icons/arrow-right.svg"
-              alt="Arrow Right"
-              width={20}
-              height={20}
-            />
-          </Button>
+          <div className="w-full h-fit flex justify-center my-10 md:justify-start items-center md:items-start">
+            <Button className="bg-[#BEF264] text-black w-32 md:w-56 h-10 md:h-16 text-base md:text-lg font-bold flex items-center gap-x-2">
+              <span>Use Pulse</span>
+              <Image
+                src="/icons/arrow-right.svg"
+                alt="Arrow Right"
+                width={20}
+                height={20}
+                className="hidden md:block"
+              />
+              <Image
+                src="/icons/arrow-right.svg"
+                alt="Arrow Right"
+                width={10}
+                height={10}
+                className="block md:hidden"
+              />
+            </Button>
+          </div>
         </div>
-        <div className="relative w-1/2 h-full flex justify-end items-center">
+
+        <div className="relative w-full md:w-1/2 h-full mb-10 flex justify-center md:justify-end items-center">
           <Image
             src="/images/hero-image.jpg"
             alt="Doctor"
             width={400}
             height={400}
+            className="relative z-10 hidden md:block"
+          />
+          <Image
+            src="/images/hero-image.jpg"
+            alt="Doctor"
+            width={250}
+            height={250}
+            className="relative z-10 block md:hidden"
+          />
+
+          <Image
+            src="/icons/hero-patient.svg"
+            alt="Doctor"
+            width={300}
+            height={300}
+            className="absolute bottom-15 left-[5%] sm:left-[10%] md:left-[20%] lg:left-[30%] z-20 transition-all duration-300 hidden md:block"
           />
           <Image
             src="/icons/hero-patient.svg"
             alt="Doctor"
-            width={400}
-            height={400}
-            className="absolute bottom-30 left-30"
+            width={200}
+            height={100}
+            className="absolute bottom-15 -left-[3%] sm:left-[10%] md:left-[10%] lg:left-[15%] z-20 transition-all duration-300 block md:hidden"
           />
           <Image
             src="/icons/hero-doctor.svg"
             alt="Doctor"
-            width={400}
-            height={400}
-            className="absolute bottom-0 left-50"
+            width={300}
+            height={300}
+            className="absolute -bottom-10 left-[10%] sm:left-[15%] md:left-[25%] lg:left-[35%] z-20 transition-all duration-300 hidden md:block"
+          />
+          <Image
+            src="/icons/hero-doctor.svg"
+            alt="Doctor"
+            width={200}
+            height={100}
+            className="absolute -bottom-10 left-[5%] sm:left-[20%] md:left-[10%] lg:left-[15%] z-20 transition-all duration-300 block md:hidden"
           />
         </div>
       </div>
